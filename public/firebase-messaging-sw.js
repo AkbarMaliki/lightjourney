0a93631bff8d6093fdc24f9832dece588a5301c3
Maliki <39995350+AkbarMaliki@users.noreply.github.com>
2020-07-05 18:36:32 +0800

    Update firebase-messaging-sw.js
    update firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -10,14 +10,7 @@ importScripts('https://www.gstatic.com/firebasejs/7.6.2//firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.6.2//firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
 firebase.initializeApp({
-  apiKey: "AIzaSyAyvbE3CBnxqV6wh4s1xTRwjKzq5zSYtRk",
-  authDomain: "vuez-63625.firebaseapp.com",
-  databaseURL: "https://vuez-63625.firebaseio.com",
-  projectId: "vuez-63625",
-  storageBucket: "vuez-63625.appspot.com",
-  messagingSenderId: "527099963081",
-  appId: "1:527099963081:web:6422331458f6048b098f18",
-  measurementId: "G-S2B09P6X2W"
+ 
 });
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
